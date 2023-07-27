05e95cdde2caad81bc9ee3dbb5d00478b39c60f5
Leonardo Coutinho <coutinholeonardo94@gmail.com>
2023-07-27 10:31:44 -0300

    commit primeira aula

---
+++ js/exemplos.js
@@ -0,0 +1,8 @@
+let prompt = require ('prompt-sync')();
+
+let curso = prompt('Qual o nome do seu curso?');
+console.log('Curso : ' + curso);
+
+let fase = Number(prompt('Informe a fase: '));
+console.log('Fase: ' + fase);
+
